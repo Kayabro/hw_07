e0d231ebe2d47acde0a5544ae1ffaf09f00b23e8
Kayabo <griha3565478@gmail.com>
2023-02-21 10:31:43 +0300

    task_1_v1

---
+++ main.py
@@ -0,0 +1,18 @@
+#
+# string = 'пара-ра-рам рам-пам-папам па-ра-па-да '
+#
+# def search_a(text):
+#     text = text.split()
+#     counter_a = []
+#
+#     for i in text:
+#         counter_a.append(i.count('а'))
+#
+#     for i in range(1, len(counter_a)):
+#         if counter_a[i - 1] != counter_a[i]:
+#             return ('Пам парам')
+#     else:
+#         return ("Парам пам-пам")
+#
+# print(search_a(string))
+
